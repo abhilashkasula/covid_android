4e76a33e35c70a7a1293e077889ab085afe52cbc
Abhilash Kasula <58025056+abhilashkasula@users.noreply.github.com>
2021-02-02 18:39:07 +0530

    Cosmetic changes for Search

--- components/Search.js
+++ components/Search.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import {TextInput} from 'react-native';
+import styled from 'styled-components';
 
-const Search = ({text, onChange}) => (
+const Search = ({style, text, onChange}) => (
   <TextInput
     value={text}
     onChangeText={onChange}
     placeholder="Search a state here"
+    style={style}
   />
 );
 
-export default Search;
+const StyledSearch = styled(Search)`
+  border: 1px solid #ccc;
+  margin: 0 auto;
+  margin-top: 25px;
+  margin-bottom: 27px;
+  border-radius: 3px;
+  padding: 13px 28px;
+  width: 350px;
+`;
+
+export default StyledSearch;
